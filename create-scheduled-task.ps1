3038a9879e5ee8a77eb9023517fa3fdb192ec680
Bernt Anker <bernt.anker@rs.no>
2023-01-22 21:54:23 +0100

    update

--- create-scheduled-task.ps1
+++ create-scheduled-task.ps1
@@ -10,13 +10,13 @@ if (-not($scriptExist)) {
 $taskPath = "\Redningsselskapet\"
 $taskName = "Map Network Drives"
 $delay = New-TimeSpan -Seconds 30
-$trigger = New-ScheduledTaskTrigger -AtLogon -RandomDelay $delay
+$trigger = New-ScheduledTaskTrigger -AtLogon -RandomDelay $delay 
+$trigger.Delay = "PT30S"
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass $scriptPath"
 $task = Get-ScheduledTask -TaskName $taskName -TaskPath $taskPath -ErrorAction SilentlyContinue -OutVariable task
-$principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administratorer" -RunLevel Highest
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -RunOnlyIfNetworkAvailable
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
 
 if (-not($task)) {
-    Register-ScheduledTask -Action $action -Trigger $trigger -TaskPath $taskPath -TaskName $taskName -Description "Maps Local Network Drives" -Settings $settings
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskPath $taskPath -TaskName $taskName -Description "Maps Local Network Drives" -Settings $settings 
 }
 
