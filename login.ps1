d81606cf7a22d000e0a7eefae7a15b657b912839
Bernt Anker <bernt.anker@gmail.com>
2023-01-29 13:00:49 +0100

    feat: Deploy nerdfonts and term profile

--- login.ps1
+++ login.ps1
@@ -1,6 +1,6 @@
 # Login Script
 
-Write-Host "Running Redningsselskapet Login Script"
+Write-Host "Running Redningsselskapet Login Script..."
 
 # Update Scoop
 if (Test-Path "$env:USERPROFILE\scoop") { scoop update *}
