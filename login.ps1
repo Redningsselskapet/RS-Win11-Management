ca887a164e877736aff13407a97e12326bb6b82a
Bernt Anker <bernt.anker@gmail.com>
2023-01-29 23:48:00 +0100

    fix: Fix typo

--- login.ps1
+++ login.ps1
@@ -5,4 +5,4 @@ Write-Host "Running Redningsselskapet Login Script..."
 # Update Scoop
 if (Get-Command scoop -ErrorAction SilentlyContinue) { scoop update * }
 
-if (Get-Command scoop -ErrorAction SilentlyContinue) { choco upgrade -y all }
+if (Get-Command choco -ErrorAction SilentlyContinue) { choco upgrade -y all }
