118545be2e160e2336c68bc0a63c487d37986978
Bernt Anker <bernt.anker@gmail.com>
2023-01-30 15:43:44 +0100

    feat: Download TVQS

--- login.ps1
+++ login.ps1
@@ -6,3 +6,12 @@
 if (Get-Command scoop -ErrorAction SilentlyContinue) { scoop update * >> c:\rs-login.log }
 
 if (Get-Command choco -ErrorAction SilentlyContinue) { choco upgrade -y all >> c:\rs-login.log }
+
+# Download TeamViewer QuickSupport
+if (-not (Test-Path -Path "C:\Program Files (x86)\TeamViewer\TeamViewerQS.exe")) {
+    "Downloading TeamViewer QuickSupport..." >> c:\rs-login.log
+    $tvUrl = "https://get.teamviewer.com/6u3kbmg"
+    $tvPath = "C:\Program Files (x86)\TeamViewer\TeamViewerQS.exe"
+    Invoke-RestMethod $tvUrl -OutFile $tvPath
+    " - Done." >> c:\rs-login.log
+}
