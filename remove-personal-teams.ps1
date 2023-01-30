349e7da6080e2b9980a7226d027cc1e2e79794b0
Bernt Anker <bernt.anker@gmail.com>
2023-01-30 13:57:01 +0100

    fix: Removed verbose logging

--- remove-personal-teams.ps1
+++ remove-personal-teams.ps1
@@ -1,26 +1,26 @@
 "Removing Personal Teams..." >> c:\intune.log
 If ($null -eq (Get-AppxPackage -Name MicrosoftTeams -AllUsers)) {
-    Write-Output “Microsoft Teams Personal App not present” >> c:\intune.log
+    Write-Output “Microsoft Teams Personal App not present”
 }
 Else {
     Try {
         Write-Output “Removing Microsoft Teams Personal App”
         If (Get-Process msteams -ErrorAction SilentlyContinue) {
             Try {
-                Write-Output “Stopping Microsoft Teams Personal app process” >> c:\intune.log
+                Write-Output “Stopping Microsoft Teams Personal app process”
                 Stop-Process msteams -Force
-                Write-Output “Stopped” >> c:\intune.log
+                Write-Output “Stopped”
             }
             catch {
-                Write-Output “Unable to stop process, trying to remove anyway” >> c:\intune.log
+                Write-Output “Unable to stop process, trying to remove anyway”
             }
            
         }
         Get-AppxPackage -Name MicrosoftTeams -AllUsers | Remove-AppPackage -AllUsers
-        Write-Output “Microsoft Teams Personal App removed successfully” >> c:\intune.log
+        Write-Output “Microsoft Teams Personal App removed successfully”
     }
     catch {
-        Write-Error “Error removing Microsoft Teams Personal App” >> c:\intune.log
+        Write-Error “Error removing Microsoft Teams Personal App”
     }
 }
 
