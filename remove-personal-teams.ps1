b12ed0bbd5c652a161e40f78c2eafeb672d34500
Bernt Anker <bernt.anker@gmail.com>
2023-01-30 13:37:25 +0100

    feat:

--- remove-personal-teams.ps1
+++ remove-personal-teams.ps1
@@ -1,26 +1,26 @@
 "Removing Personal Teams..." >> c:\intune.log
 If ($null -eq (Get-AppxPackage -Name MicrosoftTeams -AllUsers)) {
-    Write-Output “Microsoft Teams Personal App not present”
+    Write-Output “Microsoft Teams Personal App not present” >> c:\intune.log
 }
 Else {
     Try {
         Write-Output “Removing Microsoft Teams Personal App”
         If (Get-Process msteams -ErrorAction SilentlyContinue) {
             Try {
-                Write-Output “Stopping Microsoft Teams Personal app process”
+                Write-Output “Stopping Microsoft Teams Personal app process” >> c:\intune.log
                 Stop-Process msteams -Force
-                Write-Output “Stopped”
+                Write-Output “Stopped” >> c:\intune.log
             }
             catch {
-                Write-Output “Unable to stop process, trying to remove anyway”
+                Write-Output “Unable to stop process, trying to remove anyway” >> c:\intune.log
             }
            
         }
         Get-AppxPackage -Name MicrosoftTeams -AllUsers | Remove-AppPackage -AllUsers
-        Write-Output “Microsoft Teams Personal App removed successfully”
+        Write-Output “Microsoft Teams Personal App removed successfully” >> c:\intune.log
     }
     catch {
-        Write-Error “Error removing Microsoft Teams Personal App”
+        Write-Error “Error removing Microsoft Teams Personal App” >> c:\intune.log
     }
 }
 
