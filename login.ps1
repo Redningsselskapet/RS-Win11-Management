63d47e0841c1b827baef91b1ceac340b62e7e905
Bernt Anker <bernt.anker@gmail.com>
2023-01-31 18:01:07 +0100

    fix: do not run if not admin
    Do not run part of login  script that needs admin access

--- login.ps1
+++ login.ps1
@@ -1,9 +1,24 @@
 # Login Script
 
-"Running Redningsselskapet Login Script..." > c:\rs-login.log
+"Running Redningsselskapet Login Script..." > c:\temp\rs-login.log
 
-# Update Scoop
-if (Get-Command scoop -ErrorAction SilentlyContinue) { scoop update * >> c:\rs-login.log }
+# Set the default location for the log file
+$logPath = "c:\temp\rs-login.log"
 
-if (Get-Command choco -ErrorAction SilentlyContinue) { choco upgrade -y all >> c:\rs-login.log }
+# Update Scoop packages
+Write-Information "Updating Scoop packages..." >> $logPath
+if (Get-Command scoop -ErrorAction SilentlyContinue) { scoop update * >> $logPath }
+
+# Checking for elevated permissions. If not, break.
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Insufficient permissions to run this part of login script. You are probably not the administrator of this PC." >> $logPath;
+    Break
+}
+else {
+    Write-Host "Elevated permissions detected. Continuing..." -ForegroundColor Green; >> $logPath;
+    # Update Chocolatey Packages
+    Write-Information "Updating Chocolatey packages..." >> $logPath
+    if (Get-Command choco -ErrorAction SilentlyContinue) { choco upgrade -y all >> $logPath }
+}
+
 
