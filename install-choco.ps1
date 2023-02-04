2f01e715f23e1931610909eead844643b3faea14
Bernt <bernt.anker@rs.no>
2023-02-05 00:07:20 +0100

    feat: Install choco if not exist. git and volta moved to devtools

--- install-choco.ps1
+++ install-choco.ps1
@@ -1,11 +1,11 @@
-"Install Chocolatey and packages..." >> c:\intune.log
+"Install Chocolatey and standard packages..." >> c:\intune.log
 # Install Chocolatey
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+if (-not(Get-Command choco -ErrorAction SilentlyContinue)) {
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
 
 # Install Chocolatey packages
-choco install -y git
 choco install -y starship
-choco install -y volta
 choco install -y cascadia-code-nerd-font
 
 " - Done." >> c:\intune.log
