0134e591c90352b51f13d0b7a7c02444b8ada6f7
Bernt Anker <bernt.anker@rs.no>
2023-02-05 20:40:12 +0100

    Better logging

--- install-choco-devtools.ps1
+++ install-choco-devtools.ps1
@@ -1,20 +1,48 @@
-"Install Chocolatey and devtools packages..." >> c:\intune.log
+Write-Host "Install Chocolatey and devtools packages..." >> c:\intune.log
 # Install Chocolatey
 if (-not(Get-Command choco -ErrorAction SilentlyContinue)) {
-    "Installing choco.." >> c:\intune.log
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) 
 }
 
 # Install Chocolatey packages
-choco install -y git >> c:\intune.log
-choco install -y volta >> c:\intune.log
-choco install -y vscode >> c:\intune.log
-choco install -y kubernetes-cli >> c:\intune.log
-choco install -y kubectx >> c:\intune.log
-choco install -y kubens >> c:\intune.log
-choco install -y k9s >> c:\intune.log
-choco install -y azure-cli >> c:\intune.log
-choco install -y lens >> c:\intune.log
-choco install -y gitkraken >> c:\intune.log
+Write-Host -NoNewline " - Installing git..." >> c:\intune.log
+choco install -y git
+Write-Host "Done" >> c:\intune.log
 
-" - Done." >> c:\intune.log
+Write-Host -NoNewline " - Installing volta..." >> c:\intune.log
+choco install -y volta 
+Write-Host "Done" >> c:\intune.log
+
+Write-Host -NoNewline " - Installing vscode..." >> c:\intune.log
+choco install -y vscode 
+Write-Host "Done" >> c:\intune.log
+
+Write-Host -NoNewline " - Installing kubernetes-cli..." >> c:\intune.log
+choco install -y kubernetes-cli 
+Write-Host "Done" >> c:\intune.log
+
+Write-Host -NoNewline " - Installing kubectx..." >> c:\intune.log
+choco install -y kubectx
+Write-Host "Done" >> c:\intune.log
+
+Write-Host -NoNewline " - Installing kubens..." >> c:\intune.log
+choco install -y kubens 
+Write-Host "Done" >> c:\intune.log
+
+Write-Host -NoNewline " - Installing k9s..." >> c:\intune.log
+choco install -y k9s
+Write-Host "Done" >> c:\intune.log
+
+Write-Host -NoNewline " - Installing azure-cli..." >> c:\intune.log
+choco install -y azure-cli 
+Write-Host "Done" >> c:\intune.log
+
+Write-Host -NoNewline " - Installing lens..." >> c:\intune.log
+choco install -y lens
+Write-Host "Done" >> c:\intune.log
+
+Write-Host -NoNewline " - Installing gitkraken..." >> c:\intune.log
+choco install -y gitkraken
+Write-Host "Done" >> c:\intune.log
+
+Write-Host "Done" >> c:\intune.log
--- install-choco.ps1
+++ install-choco.ps1
@@ -1,12 +1,23 @@
-"Install Chocolatey and standard packages..." >> c:\intune.log
+Write-Host "Install Chocolatey and standard packages..." >> c:\intune.log
 # Install Chocolatey
 if (-not(Get-Command choco -ErrorAction SilentlyContinue)) {
+    Write-Host -NoNewline " - Installing starship..."
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Write-Host "Done"
 }
 
 # Install Chocolatey packages
+Write-Host -NoNewline " - Installing starship..." >> c:\intune.log
 choco install -y starship
-choco install -y cascadia-code-nerd-font
-choco install -y gsudo
+Write-Host "Done" >> c:\intune.log
 
-" - Done." >> c:\intune.log
+Write-Host -NoNewline " - cascadia-code-nerd-font..." >> c:\intune.log
+choco install -y cascadia-code-nerd-font
+Write-Host "Done" >> c:\intune.log
+
+Write-Host -NoNewline " - Installing gsudo..." >> c:\intune.log
+choco install -y gsudo
+Write-Host "Done" >> c:\intune.log
+
+Write-Host "Done." >> c:\intune.log
+
